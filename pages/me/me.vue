a7e14e5ed254803bb573755cdc5ab30930748fba
fmomos <2455669576@qq.com>
2024-08-22 11:47:24 +0800

    关注，我的界面

---
+++ pages/me/me.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="container">
+		<!-- 用户信息部分 -->
+		<view class="myinfo">
+			<image class="myavatar" :src="user.avatar"></image>
+			<text class="mynickname">{{ user.nickname }}</text>
+			<button size="default" style="background-color:#ffb9c0;color: #000000" @click="goToProfile">去设置</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
+				user: {
+					avatar: '/static/c1.png',
+					nickname: '1111'
+				}
+			}
+		},
+		methods: {
+	
+		}
+	}
+	
+</script>
+
+<style>
+	.myinfo {
+		display: flex;
+		align-items: center;
+		padding: 50rpx;
+		background-image: linear-gradient(to top left, #ffcbcc, #ffe4e6);
+	}
+
+	.myavatar {
+		width: 150rpx;
+		height: 150rpx;
+		border-radius: 50%;
+		margin-right: 50rpx;
+	}
+
+	.mynickname {
+		font-size: 28rpx;
+		color: #333;
+	}
+</style>
---
+++ pages/me/setting.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<text>设置111</text>
+		<button @click="gotop">去我的页面</button>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		methods:{
+			gotop(){
+				uni.navigateTo({
+					url:'/pages/me/concern'
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
