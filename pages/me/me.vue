dc724ac303c5543dd93ad2848c6d953775cdd07e
fmomos <2455669576@qq.com>
2024-08-22 19:35:24 +0800

    新建info

--- pages/me/me.vue
+++ pages/me/me.vue
@@ -4,86 +4,9 @@
 		<view class="myinfo">
 			<image class="myavatar" :src="user.avatar"></image>
 			<text class="mynickname">{{ user.nickname }}</text>
-			<button size="default" style="background-color:#ffb9c0;color: #000000" @click="goToProfile">去设置</button>
+			<button size="mini" style="background-color:#ffb9c0;color: #000000" @click="goToProfile">去设置</button>
 		</view>
-	<!-- 我们根据某一个状态值进行不同界面切换 -->
-	<view v-if="logined">
-		<u-navbar :is-back="false" title="　" :border-bottom="false">
-			<view class="u-flex u-row-right" style="width: 100%;">
-				<view class="camera u-flex u-row-center">
-					<u-icon name="camera-fill" color="#000000" size="48"></u-icon>
-				</view>
-			</view>
-		</u-navbar>
-		<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
-			<view class="u-m-r-10">
-				<u-avatar :src="pic" size="140"></u-avatar>
-			</view>
-			<view class="u-flex-1">
-				<view class="u-font-18 u-p-b-20">uView ui</view>
-				<view class="u-font-14 u-tips-color">微信号:helang_uView</view>
-			</view>
-			<view class="u-m-l-10 u-p-10">
-				<u-icon name="scan" color="#969799" size="28"></u-icon>
-			</view>
-			<view class="u-m-l-10 u-p-10">
-				<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
-			</view>
-		</view>
-		
-		<view class="u-m-t-20">
-			<u-cell-group>
-				<u-cell-item icon="rmb-circle" title="支付"></u-cell-item>
-			</u-cell-group>
-		</view>
-		
-		<view class="u-m-t-20">
-			<u-cell-group>
-				<u-cell-item icon="star" title="收藏"></u-cell-item>
-				<u-cell-item icon="photo" title="相册"></u-cell-item>
-				<u-cell-item icon="coupon" title="卡券"></u-cell-item>
-				<u-cell-item icon="heart" title="关注"></u-cell-item>
-			</u-cell-group>
-		</view>
-		
-		<view class="u-m-t-20">
-			<u-cell-group>
-				<u-cell-item icon="setting" title="设置"></u-cell-item>
-			</u-cell-group>
-		</view>
-	</view>
-	<view v-else>
-		<u-navbar :is-back="false" title="　" :border-bottom="false">
-			<view class="u-flex u-row-right" style="width: 100%;">
-				<view class="camera u-flex u-row-center">
-					<u-icon name="camera-fill" color="#000000" size="48"></u-icon>
-				</view>
-			</view>
-		</u-navbar>
-		<view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
-			<view class="u-m-r-10">
-				<u-avatar :src="pic" size="140"></u-avatar>
-			</view>
-			<view class="u-flex-1">
-				<view class="u-font-18 u-p-b-20">未登录</view>
-				<view class="u-font-14 u-tips-color"></view>
-			</view>
-			<view class="u-m-l-10 u-p-10">
-				<u-icon name="scan" color="#969799" size="28"></u-icon>
-			</view>
-			<view class="u-m-l-10 u-p-10">
-				<u-icon name="arrow-right" color="#969799" size="28"></u-icon>
-			</view>
-		</view>
-		
-		
-		<view class="u-m-t-20">
-			<u-cell-group>
-				<!-- 点击事件 -->
-				<u-cell-item icon="man-add" title="登录" @click="login()"></u-cell-item>
-			</u-cell-group>
-		</view>
-	  </view>
+		<!-- 我们根据某一个状态值进行不同界面切换 -->
 	</view>
 </template>
 
@@ -96,21 +19,53 @@
 					avatar: '/static/c1.png',
 					nickname: '1111'
 				},
-				pic:'https://uviewui.com/common/logo.png',
-				show:true,
+				pic: 'https://uviewui.com/common/logo.png',
+				show: true,
 				// 定义一个变量
-				logined:false
+				logined: false
 			}
 		},
 		onLoad() {
-			
+
 		},
 		methods: {
-			login(){
+			fetchUser() {
+				uni.request({
+					url: "http://127.0.0.1:4523/m1/5010181-4669608-default/user/me", //在fetchUserList方法中的请求URL使用了'${apiEndpoint}?page=${page}'，这会导致字符串直接拼接而非动态生成变量。正确的做法应该是使用模板字符串的语法来动态替换变量
+					data:this.user,
+					method: 'GET',
+					success: (res) => {
+						if (res.statusCode === 200) {
+						  console.log(res)
+							}
+						else {
+							uni.showToast({
+								title: '获取数据失败',
+								icon: 'none'
+							});
+						}
+					},
+					fail: (err) => {
+						uni.showToast({
+							title: '请求失败',
+							icon: 'none'
+						});
+					},
+					complete: () => {
+						this.loading = false;
+					}
+				});
+			},
+			goToProfile() {
+				uni.navigateTo({
+					url: '/pages/me/setting'
+				});
+			},
+			login() {
 				console.log("登录")
 				//跳转到登录页面
 				uni.navigateTo({
-					url:"/pages/me/login"
+					url: "/pages/me/login"
 				})
 			}
 		}
@@ -137,22 +92,23 @@
 		color: #333;
 	}
 </style>
-				
+
 
 <style lang="scss">
-page{
-	background-color: #ededed;
-}
-
-.camera{
-	width: 54px;
-	height: 44px;
-	
-	&:active{
+	page {
 		background-color: #ededed;
 	}
-}
-.user-box{
-	background-color: #fff;
-}
+
+	.camera {
+		width: 54px;
+		height: 44px;
+
+		&:active {
+			background-color: #ededed;
+		}
+	}
+
+	.user-box {
+		background-color: #fff;
+	}
 </style>
