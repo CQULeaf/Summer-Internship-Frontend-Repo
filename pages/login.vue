ed516b965b4dccb9a250e9508914968e7c6a8b94
fmomos <2455669576@qq.com>
2024-08-26 14:17:15 +0800

    关注

--- pages/login.vue
+++ pages/login.vue
@@ -5,8 +5,10 @@
 				<div class="login-wrapper">
 					<div class="header">登录</div>
 					<div class="form-wrapper">
-						<input type="text" name="username" placeholder="请输入用户名" class="input-item" v-model="user.username">
-						<input type="password" name="password" placeholder="请输入密码" class="input-item" v-model="user.password">
+						<input type="text" name="username" placeholder="请输入用户名" class="input-item"
+							v-model="user.username">
+						<input type="password" name="password" placeholder="请输入密码" class="input-item"
+							v-model="user.password">
 						<div class="btn" @click="login">登录</div>
 						<div class="btn" @click="register">注册</div>
 					</div>
@@ -17,31 +19,30 @@
 </template>
 
 <script>
-    export default {
-		data()
-		{
-			return{
-				user:{
-					username:"",
-					password:""
+	export default {
+		data() {
+			return {
+				user: {
+					username: "",
+					password: ""
 				}
 			}
 		},
-		
-		methods:{
-			login:function(){
-				if(this.user.username==''){
+
+		methods: {
+			login: function() {
+				if (this.user.username == '') {
 					this.$u.toast("请输入账号");
 					return
-				}else if(this.user.password==''){
+				} else if (this.user.password == '') {
 					this.$u.toast("请输入密码");
 					return
 				}
 				uni.request({
-					url:'http://localhost:8080/user/login',
-					data:this.user,
-					method:"POST",
-					header:{
+					url: 'http://localhost:8080/user/login',
+					data: this.user,
+					method: "POST",
+					header: {
 						'Content-Type': 'application/json'
 					},
 					success: (res) => {
@@ -51,14 +52,12 @@
 							data: res.data,
 							success: () => {
 								var value = uni.getStorageSync('nowAccount');
-								console.log(value)
-								if (value.code==200){
+								console.log("存储的账户",value)
+								if (value.code == 200) {
 									uni.switchTab({
 										url: '/pages/me/mypage'
 									});
-								} 
-								else
-								{
+								} else {
 									this.$u.toast("用户名或密码错误");
 								}
 							}
@@ -67,84 +66,91 @@
 				});
 
 			},
-			
-			register(){
+
+			register() {
 				console.log("注册")
 				uni.navigateTo({
-					url:"/pages/register"
+					url: "/pages/register"
 				})
 			}
 		},
-		
-		onShow(){
+
+		onShow() {
 			uni.removeStorage({
 				key: 'nowAccount',
 			});
 		}
-    }
+	}
 </script>
 
 <style scoped>
+	html {
+		height: 100%;
+	}
 
-html {
-    height: 100%;
-}
-body {
-    height: 200%;
-}
-.container {
-    height: 100vh;
-    width: 100%;
-    background-color: #fed6dc;
-}
-.login-wrapper {
-    background-color: #fff;
-    width: 400rpx;
-    height: 1100rpx;
-    border-radius: 15rpx;
-    padding: 0 50px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-}
-.header {
-    font-size: 85rpx;
-    font-weight: normal;
-    text-align: center;
-    line-height: 200px;
-	color: #adaba8;
-}
-.input-item {
-    display: block;
-    width: 90%;
-    margin-bottom: 20px;
-    border: 0;
-    padding: 10px;
-    border-bottom: 1px solid rgb(164, 165, 155);
-    font-size: 15px;
-    outline: none;
-}
-.input-item:placeholder {
-    text-transform: uppercase;
-}
-.btn {
-    text-align: center;
-    padding: 10px;
-    margin: 0 auto;
-    width: 90%;
-    margin-top: 45px;
-	margin-bottom: -30px;
-    background-color: #fecdd4;
-    color: #fff;
-}
-.msg {
-    text-align: center;
-    line-height: 88px;
-}
-a {
-    text-decoration-line: none;
-    color: #fed6dc;
-}
+	body {
+		height: 200%;
+	}
 
+	.container {
+		height: 100vh;
+		width: 100%;
+		background-color: #fed6dc;
+	}
+
+	.login-wrapper {
+		background-color: #fff;
+		width: 400rpx;
+		height: 1100rpx;
+		border-radius: 15rpx;
+		padding: 0 50px;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.header {
+		font-size: 85rpx;
+		font-weight: normal;
+		text-align: center;
+		line-height: 200px;
+		color: #adaba8;
+	}
+
+	.input-item {
+		display: block;
+		width: 90%;
+		margin-bottom: 20px;
+		border: 0;
+		padding: 10px;
+		border-bottom: 1px solid rgb(164, 165, 155);
+		font-size: 15px;
+		outline: none;
+	}
+
+	.input-item:placeholder {
+		text-transform: uppercase;
+	}
+
+	.btn {
+		text-align: center;
+		padding: 10px;
+		margin: 0 auto;
+		width: 90%;
+		margin-top: 45px;
+		margin-bottom: -30px;
+		background-color: #fecdd4;
+		color: #fff;
+	}
+
+	.msg {
+		text-align: center;
+		line-height: 88px;
+	}
+
+	a {
+		text-decoration-line: none;
+		color: #fed6dc;
+	}
 </style>
